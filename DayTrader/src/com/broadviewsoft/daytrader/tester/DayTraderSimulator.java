9e49134c961390f8ed1360421a490b7be99818c0
ginchang <ginchang@b8f34ae5-6a59-4d23-b9e9-9b65760df09e>
2018-02-25 23:06:25 +0000

    moved around constants file

--- DayTrader/src/com/broadviewsoft/daytrader/tester/DayTraderSimulator.java
+++ DayTrader/src/com/broadviewsoft/daytrader/tester/DayTraderSimulator.java
@@ -9,7 +9,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.broadviewsoft.daytrader.domain.Account;
-import com.broadviewsoft.daytrader.domain.Constants;
+import com.broadviewsoft.daytrader.util.Constants;
 import com.broadviewsoft.daytrader.service.TradePlatform;
 import com.broadviewsoft.daytrader.service.ITradeStrategy;
 import com.broadviewsoft.daytrader.service.impl.CciStrategy;
@@ -25,7 +25,7 @@ import com.broadviewsoft.daytrader.service.impl.RsiStrategy;
  * <B>Creation date:</B> Mar 8, 2013 3:43:24 PM
  * </P>
  * 
- * @author Jason Zhang
+ * @author Jingda Zhang
  */
 public class DayTraderSimulator {
 	private static Log logger = LogFactory.getLog(DayTraderSimulator.class);
@@ -51,10 +51,12 @@ public class DayTraderSimulator {
 	}
 	
 	public void simulate(Account account, String symbol, Date startDate, Date endDate) {
+		logger.info("Starting simulation...");
 		for (ITradeStrategy strategy : strategies) {
 			logger.info("Applying strategy (" + strategy.getDescription() + ") for " + symbol);
 			tradePlatform.trade(account, strategy, symbol, startDate, endDate);
 		}
+		logger.info("Done simulation.");
 	}
 
 	public static void main(String[] args) throws ParseException {
@@ -64,8 +66,8 @@ public class DayTraderSimulator {
 //		simulator.addStrategies(new RsiStrategy());
 		String[] symbols = Constants.INIT_STOCK_SYMBOLS;
 
-		Date startDate = Constants.TRADE_DATE_FORMATTER.parse("04/04/2013");
-		Date endDate = Constants.TRADE_DATE_FORMATTER.parse("04/08/2013");
+		Date startDate = Constants.TRADE_DATE_FORMATTER.parse("04/19/2013");
+		Date endDate = Constants.TRADE_DATE_FORMATTER.parse("04/20/2013");
 
 		for (String symbol : symbols) {
 		  simulator.simulate(account, symbol, startDate, endDate);
